9b4736a20d0d5a2710fdb901331f840e7b30f8d0
Faduma <aladirie@gmail.com>
2023-11-13 18:38:56 -0500

    created character counter for tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    const tweets = 140;
+    
+    $('.new-tweet textarea').on('input', function() {
+      const inputText = $(this).val().length;
+      console.log (inputText);
+      const tweetlength = tweets - inputText;
+      const characterCounter = $(`output.counter`);
+      characterCounter.text(tweetlength);
+     
+
+      if (tweetlength > 0) {
+        characterCounter.addClass("exceeded-limit");
+      } else {
+        characterCounter.removeClass("exceeded-limit");
+      }
+    });
+  });
+  
