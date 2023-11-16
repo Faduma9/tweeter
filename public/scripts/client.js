5dccfa4b3c058de5897cbf7657c0e9d6dd606283
Faduma <aladirie@gmail.com>
2023-11-16 02:21:36 -0500

    Implement client-side validation to display error messages for excessive tweet length or empty content, ensuring the form remains uncleared and submission is prevented.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,12 +5,24 @@
  */
 
 $(document).ready(function() {
-  // Add an event listener for the form submit event
   $('form').submit(function(event) {
-    // Prevent the default form submission behavior
-    event.preventDefault();
+    const tweetContent = $('#tweet-text').val(); // Get the tweet content
 
-    // Serialize the form data
+    // Validate tweet presence
+    if (tweetContent.trim() === '') {
+      alert('Tweet content is required.');
+      event.preventDefault(); // Stop form submission
+      return; // Exit out of the function immediately.
+    }
+
+    // Validate tweet length
+    else if (tweetContent.length > 140) {
+      alert('Tweet is too long. Maximum length is 140 characters.');
+      event.preventDefault(); // Stop form submission
+      return; // Exit out of the function immediately.
+    }
+
+    // If validations pass, serialize the form data
     const formData = $(this).serialize();
     console.log(formData);
 
@@ -23,7 +35,6 @@ $(document).ready(function() {
         // Fetch and render tweets after posting
         fetchTweets();
 
-       
         alert('Tweet posted successfully!');
       })
       .fail(function(error) {
@@ -33,28 +44,32 @@ $(document).ready(function() {
         // Display an error message to the user
         alert('Error posting tweet. Please try again.');
       });
+
+    // Prevent the default form submission to avoid page reload
+    event.preventDefault();
   });
 
-  // Function to fetch tweets from the server and render them
-  const fetchTweets = function() {
-    // endpoint to fetch tweets from the server
-    $.get('/tweets')
-      .done(function(tweets) {
-        // Render the fetched tweets
-        renderTweets(tweets);
 
-        // Apply timeago to the new tweets
-        $(".timestamp").timeago();
-      })
-      .fail(function(error) {
-        // Handle the error response as needed
-        console.error(error);
-      });
-  };
+// Function to fetch tweets from the server and render them
+const fetchTweets = function() {
+  // endpoint to fetch tweets from the server
+  $.get('/tweets')
+    .done(function(tweets) {
+      // Render the fetched tweets
+      renderTweets(tweets);
 
-  // Function to create a tweet element dynamically
-  const createTweetElement = function(tweet) {
-    const $tweet = $(`
+      // Apply timeago to the new tweets
+      $(".timestamp").timeago();
+    })
+    .fail(function(error) {
+      // Handle the error response as needed
+      console.error(error);
+    });
+};
+
+// Function to create a tweet element dynamically
+const createTweetElement = function(tweet) {
+  const $tweet = $(`
       <article class="tweet">
         <header>
         <img src="${tweet.user.avatars}" alt="Profile Image"> <!-- Use the user's profile image URL -->
@@ -79,50 +94,50 @@ $(document).ready(function() {
       </article>
     `);
 
-    return $tweet;
-  };
+  return $tweet;
+};
 
-  // Function to render an array of tweets dynamically
-  const renderTweets = function(tweets) {
-    // Clear the existing tweets in the container before rendering new ones
-    $(".tweet-container").empty();
+// Function to render an array of tweets dynamically
+const renderTweets = function(tweets) {
+  // Clear the existing tweets in the container before rendering new ones
+  $(".tweet-container").empty();
 
-    // Loop through the tweets array and append each tweet to the container
-    for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $(".tweet-container").append($tweet);
+  // Loop through the tweets array and append each tweet to the container
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $(".tweet-container").append($tweet);
 
-      // Use timeago to format and update the timestamp
-      $(".timestamp", $tweet).timeago();
-    }
-  };
+    // Use timeago to format and update the timestamp
+    $(".timestamp", $tweet).timeago();
+  }
+};
 
-  // Initial tweet data
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
+// Initial tweet data
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
     },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
-  // Call renderTweets to dynamically render the tweets
-  renderTweets(data);
+// Call renderTweets to dynamically render the tweets
+renderTweets(data);
 });
